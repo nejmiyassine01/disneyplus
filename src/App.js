14c4077ef1cd28690b93ed32270bbb8867fc1979
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-04-28 23:16:40 +0000

    Project Complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header.component";
 import Login from "./components/Login.component";
 import Home from "./components/Home.component";
+import Detail from "./components/Detail.component";
 
 const App = () => {
 	return (
@@ -12,6 +13,7 @@ const App = () => {
 				<Switch>
 					<Route exact path="/" component={Login} />
 					<Route path="/home" component={Home} />
+					<Route path="/detail/:id" component={Detail} />
 				</Switch>
 			</Router>
 		</div>
